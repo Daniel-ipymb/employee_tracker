373f1a6f95c0e515a210f129a4dfbc72fc2a0e94
Daniel O <danielogunlana23@gmail.com>
2021-06-27 00:55:16 -0400

    inserted data into table

--- employee.sql
+++ employee.sql
@@ -5,45 +5,36 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE roless (
+INSERT INTO department(name)
+VALUES('Management'), ('Sales'), ('Engineering'), ('finance'),('Legal')
+
+
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  salary DECIMAL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
   department_id INT,
   PRIMARY KEY(id)
 );
 
+
+INSERT INTO role(title, salary, department_id)
+VALUES('Sales Lead', 12000, 2), ('Salesperson', 80000, 2), ('Lead Engineer', 150000, , 3), ('Software Engineer', 120000, 3), ('Accountant', 125000, 4), ('Entrepreneur', 200000, 1)
+
+
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
 );
 
 
-INSERT INTO department(name)
-VALUES('Management'), ('Sales'), ('Engineering'), ('finance'),
-
-INSERT INTO roles()
-
-INSERT INTO employees
-
-
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
-
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
-
--- ### Alternative way to insert more than one row
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES('John', 'Adams', 1, 3), ('Mike', 'Epps', 2, 6), ('kevin', 'Gate', 3, NULL), ('Ben', 'Smalls', 4, 6), ('Tom', 'Brady', 5, 3), ('Chirstian', 'Rodriguez', 6, NULL), ('Ashley', 'Withers', 2, 3);
